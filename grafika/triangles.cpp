151c8c49fa8b7fc0b59e80c650bfcc627f7475b0
T0liver <taschnero@edu.bme.hu>
2025-03-05 09:33:39 +0100

    Answer the questions Write the answers for the third chapter in the pdf in comments.

--- grafika/triangles.cpp
+++ grafika/triangles.cpp
@@ -1,36 +1,9 @@
+/*
+
 //=============================================================================================
-// Grafika 1. házifeladat.
+// Zöld háromszög: A framework.h osztályait felhasználó megoldás
 //=============================================================================================
 #include "framework.h"
-
-///===========
-/// 1. Egyenletek
-/// 2D egyenes implicit (normálvektoros) egyenlete: Ax + By + C = 0
-/// 2D egyenes paraméteres egyenletei: x = x0 + t * dx	;	y = y0 + t * dy
-/// paraméteres egyenlet paraméterei => implicit: A = dy, B = -dx, C = - (Ax0 + By0) = dx * y0 - dy * x0
-/// implicit egyenlet => paraméteres: (dx, dy) = (B, -A), (x0, y0) = 
-/// egyenlet két pontból: P1(x1, y1); P2(x2, y2);	A = y1 - y2, B = x2 - x1, C = y2 * x1 - y1 * x2
-/// ======
-/// 2. Metszéspont képletek
-/// a) x = (B1 * C1 - B2 * C1) / (A1 * B2 - A2 * B1)	;	y = (A2 * C1 - A1 * C2) / (A1 * B2 - A2 * B1)
-/// b) t1 = ((x20 - x10) + t2 * a2)/a1 ;	t2 = ((y10 - y20) + ((x20 - x10) * b1)/a1) / (b2 - (a2 * b1)/a1)
-/// c) x = x0 + t * dx	;	y = y0 + t * dy
-/// ======
-/// 3. Ne essen a (-1, -1);(1, 1) négyzetbe az egyenes két pontja
-/// P1(x1,y1) legyen a négyzet bal oldalán; P2(x2,y2) legyen a négyzet jobb oldalán
-/// jó ha !(1 > x1 > -1 és 1 > y1 > -1 és 1 > x2 > -2 és 1 > y2 > -1)
-/// ======
-/// 4. Pont és egyenes távolsága
-/// implicit egyenlettel:		d = |A * x0 + B * y0 + C| / sqrt(A^2 + B^2)
-/// paraméteres egyenlettel:	d = |(py - y0) * dx - (px - x0) * dy| / sqrt(dx^2 + dy^2)
-/// ======
-/// 5. Teglalap sarkai
-/// Adottak tetszoleges P1(x1, y1) és P2(x2, y2) pontok.
-/// Elso sarok:		x1, -y1
-/// Masodik sarok:	x2, -y2
-/// Harmadik sarok:	-x1, y2
-/// Negyedik sarok:	-x1, y2
-/// ===========
 
 // csúcspont árnyaló
 const char* vertSource = R"(
@@ -58,14 +31,6 @@ const char* fragSource = R"(
 )";
 
 const int winWidth = 600, winHeight = 600;
-
-const int pointSize = 10, lineSize = 3;
-
-
-class Object {
-public:
-	virtual void Draw(GPUProgram* gpuProgram, GLenum mode, const vec3& color) = 0;
-};
 
 class GreenTriangleApp : public glApp {
 	Geometry<vec2>* triangle;  // geometria
@@ -97,3 +62,4 @@ public:
 
 GreenTriangleApp app;
 
+*/

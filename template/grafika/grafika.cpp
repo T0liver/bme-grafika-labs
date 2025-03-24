38a8ad27f3fd61d32a327a027522be73d758e622
T0liver <taschnero@edu.bme.hu>
2025-03-24 11:44:28 +0100

    Add stating proj files

---
+++ template/grafika/grafika.cpp
@@ -0,0 +1,52 @@
+//=============================================================================================
+// Alap grafika kód: A framework.h osztályait felhasználó megoldás
+//=============================================================================================
+#include "framework.h"
+
+// csúcspont árnyaló
+const char* vertSource = R"(
+	#version 330				
+    precision highp float;
+
+	layout(location = 0) in vec2 cP;	// 0. bemeneti regiszter
+
+	void main() {
+		gl_Position = vec4(cP.x, cP.y, 0, 1); 	// bemenet már normalizált eszközkoordinátákban
+	}
+)";
+
+// pixel árnyaló
+const char* fragSource = R"(
+	#version 330
+    precision highp float;
+
+	uniform vec3 color;			// konstans szín
+	out vec4 fragmentColor;		// pixel szín
+
+	void main() {
+		fragmentColor = vec4(color, 1); // RGB -> RGBA
+	}
+)";
+
+const int winWidth = 600, winHeight = 600;
+
+class GrafikaApp : public glApp {
+	GPUProgram* gpuProgram;	   // csúcspont és pixel árnyalók
+public:
+	GrafikaApp() : glApp("Grafika") {}
+
+	// Inicializáció, 
+	void onInitialization() {
+		gpuProgram = new GPUProgram(vertSource, fragSource);
+	}
+
+	// Ablak újrarajzolás
+	void onDisplay() {
+		glClearColor(0, 0, 0, 0);     // háttér szín
+		glClear(GL_COLOR_BUFFER_BIT); // rasztertár törlés
+		glViewport(0, 0, winWidth, winHeight);
+	}
+};
+
+GrafikaApp app;
+

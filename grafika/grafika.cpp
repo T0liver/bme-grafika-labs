7a326434a790ef4087e77524fb80789f894dc03f
T0liver <taschnero@edu.bme.hu>
2025-04-02 14:16:43 +0200

    Big commit I was lazy... Adding texture map class Adding Station (points) class

--- grafika/grafika.cpp
+++ grafika/grafika.cpp
@@ -8,10 +8,22 @@ const char* vertSource = R"(
 	#version 330
 	precision highp float;
 
-	layout(location = 0) in vec2 cP;	// 0. bemeneti regiszter
-	uniform mat4 mvp;		// modelnézettér projekciós transzformáció
+	layout(location = 0) in vec2 cP;
+	layout(location = 1) in vec2 vertexUV;
+
+	out vec2 texCoord;
+
+	uniform mat4 mvp;
+	uniform bool needMvp;
+
 	void main() {
-		gl_Position = mvp * vec4(cP.x, cP.y, 0, 1); 	// bemenet már normalizált eszközkoordinátákban
+		if (needMvp) {
+			gl_Position = mvp * vec4(cP.x, cP.y, 0, 1);
+		} else {
+			gl_Position = vec4(cP.x, cP.y, 0, 1);
+		}
+		texCoord = vertexUV;
+
 	}
 )";
 
@@ -20,17 +32,26 @@ const char* fragSource = R"(
 	#version 330
     precision highp float;
 
-	uniform vec3 color;			// konstans szín
-	out vec4 fragmentColor;		// pixel szín
+	uniform bool point;
+	uniform vec3 color;
+	uniform sampler2D textureUnit;
+
+	in vec2 texCoord;
+	out vec4 fragmentColor;
 
 	void main() {
-		fragmentColor = vec4(color, 1); // RGB -> RGBA
+		if (point) {
+			fragmentColor = vec4(color, 1);
+		} else {
+			fragmentColor = texture(textureUnit, texCoord);
+		}
 	}
 )";
 
 const int winWidth = 600, winHeight = 600;
 
-// Camera class, mert gondolom kene ilyen is...
+const int lineWidth = 3, pointSize = 10;
+
 class Camera {
 	vec2 center;
 	float width, height;
@@ -56,46 +77,188 @@ public:
 	vec2 scrToW(const vec2& cords, const vec2& size) const {
 		float nX = 2.0f * cords.x / size.x - 1;
 		float nY = 1.0f - 2.0f * cords.y / size.y;
-		vec4 vertx = getViewIM() * getProjIM() * vec4(nX, nY, 0, 1);
-		return vec3(vertx.x, -vertx.y, 0.0f);
+		vec4 vertx = getProjIM() * getViewIM() * vec4(nX, nY, 0, 1);
+		return vec3(vertx.x, vertx.y, 0.0f);
 	}
 };
 
 class Object : public Geometry<vec2> {
 protected:
-	vec2 position;
+	unsigned int vao, vbo;
+	std::vector<vec2> vtx;
 public:
-	virtual std::vector<vec2> GenVertexData() = 0;
+	Object() : vao(0), vbo(0) {
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
 
-    void update() {
-		Vtx() = GenVertexData();
-		this->updateGPU();
-    }
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-	void Draw(GPUProgram* gpuProgram, int type, vec3 color, Camera& camera) {
-		// mat4 M = translate(vec3(position.x, position.y, 0.0f)) * rotate(phi, vec3(0.0f, 0.0f, 1.0f)) * scale(scaling);
-		mat4 M = translate(vec3(position.x, position.y, 0.0f)); // kellenek ezek? ^
-		mat4 MVP = camera.getProjM() * camera.getViewM() * M;
-		gpuProgram->setUniform(MVP, "MVP");
-		__super::Draw(gpuProgram, type, color);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+		glEnableVertexAttribArray(0);
+	}
+
+	void update() {
+		glBindVertexArray(vao);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, vtx.size() * sizeof(vec2), &vtx[0], GL_DYNAMIC_DRAW);
+	}
+
+	void Draw(GPUProgram* gpuProgram, const int type, const vec3 color, const mat4& mvp) {
+		if (vtx.size() > 0)
+		{
+			glBindVertexArray(vao);
+			gpuProgram->setUniform(true, "point");
+			gpuProgram->setUniform(true, "needMvp");
+			gpuProgram->setUniform(color, "color");
+			gpuProgram->setUniform(mvp, "mvp");
+			glDrawArrays(type, 0, vtx.size());
+			gpuProgram->setUniform(false, "point");
+			gpuProgram->setUniform(false, "needMvp");
+		}
+	}
+
+	~Object() {
+		glDeleteBuffers(1, &vbo);
+		glDeleteVertexArrays(1, &vao);
+	}
+};
+
+class Map {
+	std::vector<vec2> vtx;
+	unsigned int vao, vbo[2];
+	Texture* texture;
+
+public:
+	Map() {
+		std::vector<unsigned int> mapImg = {
+			252, 252, 252, 252, 252, 252, 252, 252, 252, 0, 9, 80, 1, 148, 13, 72, 13, 140, 25, 60, 21, 132, 41, 12, 1, 28, 25, 128, 61, 0, 17, 4, 29, 124, 81, 8, 37, 116, 89, 0, 69, 16, 5, 48, 97, 0, 77, 0, 25, 8, 1, 8, 253, 253, 253, 253, 101, 10, 237, 14, 237, 14, 241, 10, 141, 2, 93, 14, 121, 2, 5, 6, 93, 14, 49, 6, 57, 26, 89, 18, 41, 10, 57, 26, 89, 18, 41, 14, 1, 2, 45, 26, 89, 26, 33, 18, 57, 14, 93, 26, 33, 18, 57, 10, 93, 18, 5, 2, 33, 18, 41, 2, 5, 2, 5, 6, 89, 22, 29, 2, 1, 22, 37, 2, 1, 6, 1, 2, 97, 22, 29, 38, 45, 2, 97, 10, 1, 2, 37, 42, 17, 2, 13, 2, 5, 2, 89, 10, 49, 46, 25, 10, 101, 2, 5, 6, 37, 50, 9, 30, 89, 10, 9, 2, 37, 50, 5, 38, 81, 26, 45, 22, 17, 54, 77, 30, 41, 22, 17, 58, 1, 2, 61, 38, 65, 2, 9, 58, 69, 46, 37, 6, 1, 10, 9, 62, 65, 38, 5, 2, 33, 102, 57, 54, 33, 102, 57, 30, 1, 14, 33, 2, 9, 86, 9, 2, 21, 6, 13, 26, 5, 6, 53, 94, 29, 26, 1, 22, 29, 0, 29, 98, 5, 14, 9, 46, 1, 2, 5, 6, 5, 2, 0, 13, 0, 13, 118, 1, 2, 1, 42, 1, 4, 5, 6, 5, 2, 4, 33, 78, 1, 6, 1, 6, 1, 10, 5, 34, 1, 20, 2, 9, 2, 12, 25, 14, 5, 30, 1, 54, 13, 6, 9, 2, 1, 32, 13, 8, 37, 2, 13, 2, 1, 70, 49, 28, 13, 16, 53, 2, 1, 46, 1, 2, 1, 2, 53, 28, 17, 16, 57, 14, 1, 18, 1, 14, 1, 2, 57, 24, 13, 20, 57, 0, 2, 1, 2, 17, 0, 17, 2, 61, 0, 5, 16, 1, 28, 25, 0, 41, 2, 117, 56, 25, 0, 33, 2, 1, 2, 117, 52, 201, 48, 77, 0, 121, 40, 1, 0, 205, 8, 1, 0, 1, 12, 213, 4, 13, 12, 253, 253, 253, 141
+		};
+
+		std::vector<vec3> image = decodeTexture(mapImg, 64, 64);
+		
+		texture = new Texture(64, 64, image);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+		glGenBuffers(2, &vbo[0]);  // egyszerre két vbo-t kérünk
+		// a négyszög csúcsai kezdetben normalizált eszközkoordinátákban
+		vtx = { vec2(-1.0f, -1.0f), vec2(1.0f, -1.0f), vec2(1.0f, 1.0f), vec2(-1.0f, 1.0f) };
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]); // GPU-ra másoljuk
+		glBufferData(GL_ARRAY_BUFFER, vtx.size() * sizeof(vec2), &vtx[0], GL_STATIC_DRAW);
+		// updateGPU();               // GPU-ra másoljuk
+		glEnableVertexAttribArray(0); // a 0. vbo a 0. bemeneti regisztert táplálja
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL); // csúcsonként 2 float-tal
+		// a négyszög csúcsai textúratérben
+		std::vector<vec2> uvs = { vec2(0.0f, 0.0f), vec2(1.0f, 0.0f), vec2(1.0f, 1.0f), vec2(0.0f, 1.0f) };
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]); // GPU-ra másoljuk
+		glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(vec2), &uvs[0], GL_STATIC_DRAW);
+		glEnableVertexAttribArray(1); // a 1. vbo a 1. bemeneti regisztert táplálja
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, NULL); // csúcsonként 2 float-tal
+	}
+
+	std::vector<vec3> decodeTexture(const std::vector<uint32_t>& raw, const int width, const int height) {
+		std::vector<vec3> image;
+
+		vec3 colors[4] = {
+			vec3(1.0f, 1.0f, 1.0f),  // Fehér
+			vec3(0.0f, 0.0f, 1.0f),  // Kék
+			vec3(0.0f, 1.0f, 0.0f),  // Zöld
+			vec3(0.0f, 0.0f, 0.0f)   // Fekete
+		};
+
+		size_t i = 0, pi = 0;
+		size_t size = raw.size();
+		size_t iSize = width * height;
+		while (i < size && pi < iSize) {
+			unsigned int byte = raw.at(i++);
+			size_t rep = (byte >> 2) + 1;
+			unsigned int data = byte & 0x03;
+			for (size_t j = 0; j < rep; j++) {
+				image.push_back(colors[data]);
+			}
+		}
+		
+		return image;
+	}
+
+	void Draw(GPUProgram* gpuProgram, Camera* camera) {
+		gpuProgram->Use();
+		gpuProgram->setUniform(0, "textureUnit");
+		texture->Bind(0);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, vtx.size());
+	}
+
+	~Map() {
+		delete texture;
+	}
+};
+
+class Stations : Object {
+public:
+	Stations() : Object() {}
+
+	void add(const vec2 p) {
+		vtx.push_back(p);
+		update();
+	}
+
+	void Draw(GPUProgram* gpuProgram, Camera* camera) {
+		mat4 mvp = camera->getProjM() * camera->getViewM();
+		Object::Draw(gpuProgram, GL_POINTS, vec3(1.0f, 0.0f, 0.0f), mvp);
 	}
 };
 
 class Merkator : public glApp {
-	GPUProgram* gpuProgram;	   // csúcspont és pixel árnyalók
+	GPUProgram* gpuProgram;
+	Map* map;
+	Stations* stations;
+	Camera* camera;
 public:
 	Merkator() : glApp("Merkator térkép") {}
 
 	// Inicializáció, 
 	void onInitialization() {
 		gpuProgram = new GPUProgram(vertSource, fragSource);
+		map = new Map();
+		stations = new Stations();
+		camera = new Camera(vec2(0.0f, 0.0f), 600, 800);
+		glPointSize(pointSize);
+		glLineWidth(lineWidth);
 	}
 
 	// Ablak újrarajzolás
 	void onDisplay() {
-		glClearColor(0, 0, 0, 0);     // háttér szín
-		glClear(GL_COLOR_BUFFER_BIT); // rasztertár törlés
+		glClearColor(0, 0, 0, 0);
+		glClear(GL_COLOR_BUFFER_BIT);
 		glViewport(0, 0, winWidth, winHeight);
+
+		map->Draw(gpuProgram, camera);
+		stations->Draw(gpuProgram, camera);
+	}
+
+	void onMousePressed(MouseButton but, int pX, int pY) {
+		if (but == MOUSE_LEFT) {
+			float nX = 2.0f * pX / winWidth - 1;
+			float nY = 1.0f - 2.0f * pY / winHeight;
+
+			vec2 wPos = camera->scrToW(vec2(pX, pY), vec2(winWidth, winHeight));
+
+			printf("Clicked: %d, %d\n", pX, pY);
+			stations->add(wPos);
+			printf("Added: %f, %f\n", wPos.x, wPos.y);
+			refreshScreen();
+		}
+	}
+
+	~Merkator() {
+		delete gpuProgram;
+		delete map;
+		delete stations;
+		delete camera;
 	}
 };
 

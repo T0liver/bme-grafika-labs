e66e1dd9a9fdaa25c090db61ea213c098807e049
T0liver <taschnero@edu.bme.hu>
2025-04-28 22:00:20 +0200

    Add base classes

--- grafika/grafika.cpp
+++ grafika/grafika.cpp
@@ -38,14 +38,79 @@ struct Hit {
 
 struct Ray {
 	vec3 start, dir;
+	bool out;
 	Ray(vec3 _start, vec3 _dir) : start(_start), dir(normalize(_dir)) {}
+	Ray(vec3 _start, vec3 _dir, bool _out) : start(_start), out(_out), dir(normalize(_dir)) {}
 };
+
 
 class Intersectable {
 protected:
 	Material* material;
 public:
 	virtual Hit intersect(const Ray& ray) = 0;
+};
+
+class Sphere : public Intersectable {
+	vec3 center;
+	float radius;
+public:
+	Hit intersect(const Ray& ray) override {
+		Hit hit;
+		vec3 dist = ray.start - center;
+		float a = dot(ray.dir, ray.dir);
+		float b = dot(dist, ray.dir) * 2.0f;
+		float c = dot(dist,dist) - radius * radius;
+		float discr = b * b - 4 * a * c;
+		if (discr < 0) return hit; else discr = sqrtf(discr);
+		float t1 = (-b + discr) / (2.0f * a);
+		float t2 = (-b - discr) / (2.0f * a);
+		if (t1 <= 0) return hit;
+		hit.t = (t2 > 0) ? t2 : t1;
+		hit.position = ray.start + ray.dir * hit.t;
+		hit.normal = (hit.position - center) / radius;
+		hit.material = material;
+		return hit;
+	}
+};
+
+// TODO: Az objects változót tuti máshonnan fogja megkani, de egyenlőre maradjon így
+Hit firstIntersect(Ray ray, std::vector<Intersectable*>& objects) {  
+   Hit bestHit;  
+   for (Intersectable* obj : objects) {
+       Hit hit = obj->intersect(ray);  
+       if (hit.t > 0 && (bestHit.t < 0 || hit.t < bestHit.t)) {  
+           bestHit = hit;  
+       }  
+   }  
+   if (dot(ray.dir, bestHit.normal) > 0) {  
+       bestHit.normal *= -1;  
+   }  
+   return bestHit;  
+}
+
+class Camera {
+	vec3 eye, lookat, right, up;
+	float fov;
+public:
+	void set(vec3 _eye, vec3 _lookat, vec3 vup, float _fov) {
+		eye = _eye; lookat = _lookat; fov = _fov;
+		vec3 w = eye - lookat;
+		float windowSize = length(w) * tanf(fov / 2);
+		right = normalize(cross(vup, w)) * (float)windowSize * (float)windowWidth / (float)windowHeight;
+		up = normalize(cross(w, right)) * windowSize;
+	}
+
+	Ray getRay(int X, int Y) {
+		vec3 dir = lookat + right * (2 * (X + 0.5f) / windowWidth - 1) + up * (2 * (Y + 0.5f) / windowHeight - 1) - eye;
+		return Ray(eye, dir);
+	}
+
+	void Animate(float dt) {
+		vec3 d = eye - lookat;
+		eye = vec3(d.x * cos(dt) + d.z * sin(dt), d.y, -d.x * sin(dt) + d.z * cos(dt)) + lookat;
+		set(eye, lookat, up, fov);
+	}
 };
 
 class FullScreenTexturedQuad : public Geometry<vec2> {
